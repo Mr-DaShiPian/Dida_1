c0af003c9d8158129087f52b6eabf37687fb5997
gh-qiang <1053399091@qq.com>
2019-11-11 16:48:01 +0800

    加了个homePage主页

--- src/main/java/com/qf/Controller/StudentController.java
+++ src/main/java/com/qf/Controller/StudentController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 @RequestMapping("Student")
@@ -21,11 +24,31 @@ public class StudentController {
     public void setStudentService(StudentService studentService) {
         this.studentService = studentService;
     }
+
+    @RequestMapping("myInfor")
+    public String myInfor(){
+        return "myInfor";
+    }
+
+    //根据用户名查到Student信息传到修改页面
     @RequestMapping("getStudent")
     public String getStudent(HttpServletRequest request){
         String userName = (String) request.getSession().getAttribute("userName");
         Student student = studentService.getStudentByUserName(userName);
         request.setAttribute("student",student);
         return "updataStudent";
+    }
+
+    //保存修改内容，跳转
+    @RequestMapping("saveStudent")
+    public String saveStudent(Student student, HttpServletResponse response){
+        studentService.updateStudent(student);
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.write("修改成功！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "myInfor";
     }
 }
