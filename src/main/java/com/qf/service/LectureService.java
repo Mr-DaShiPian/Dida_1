e7905bfe1b32f904839fcd16ee2d86eaf11c50d5
niutiezhu <2036057216@qq.com>
2019-11-11 19:24:18 +0800

    讲师数据导出

--- src/main/java/com/qf/service/LectureService.java
+++ src/main/java/com/qf/service/LectureService.java
@@ -1,4 +1,10 @@
 package com.qf.service;
 
+import com.qf.pojo.Student;
+
+import java.util.List;
+
 public interface LectureService {
+    public List<Student> getStudentList();
+
 }
--- src/main/java/com/qf/service/LectureServiceImpl.java
+++ src/main/java/com/qf/service/LectureServiceImpl.java
@@ -1,4 +1,16 @@
 package com.qf.service;
 
-public class LectureServiceImpl {
+import com.qf.mapper.LectureMapper;
+import com.qf.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class LectureServiceImpl implements LectureService {
+    @Autowired
+    private LectureMapper lectureMapper;
+    @Override
+    public List<Student> getStudentList() {
+        return lectureMapper.getSudentList();
+    }
 }
