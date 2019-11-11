04eeafebb945880833aa8a09001e6bf30054a523
gh-qiang <1053399091@qq.com>
2019-11-11 15:01:55 +0800

    修改pojo

--- src/main/java/com/qf/service/StudentServiceImpl.java
+++ src/main/java/com/qf/service/StudentServiceImpl.java
@@ -1,4 +1,30 @@
 package com.qf.service;
 
-public class StudentServiceImpl {
+import com.qf.mapper.StudentMapper;
+import com.qf.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentMapper studentMapper;
+
+    public StudentMapper getStudentMapper() {
+        return studentMapper;
+    }
+
+    public void setStudentMapper(StudentMapper studentMapper) {
+        this.studentMapper = studentMapper;
+    }
+
+    @Override
+    public Student getStudentByUserName(String userName) {
+        return studentMapper.getStudentByUserName(userName);
+    }
+
+    @Override
+    public int addStudent(Student student) {
+        return studentMapper.addStudent(student);
+    }
 }
