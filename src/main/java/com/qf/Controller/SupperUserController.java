7f7a39fe0be77f17031c9b4a08889ce96cff668a
morningi <630391612@qq.com>
2019-11-12 19:12:52 +0800

    更新 课程管理的后台

--- src/main/java/com/qf/Controller/SupperUserController.java
+++ src/main/java/com/qf/Controller/SupperUserController.java
@@ -1,11 +1,40 @@
 package com.qf.Controller;
 
+import com.qf.pojo.MyClass;
+import com.qf.service.SupperUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("SupperUser")
 public class SupperUserController {
-//    @Autowired
-//    private BoosService boosService;
+    @Autowired
+    private SupperUserService supperUserService;
+
+    public SupperUserService getSupperUserService() {
+        return supperUserService;
+    }
+
+    public void setSupperUserService(SupperUserService supperUserService) {
+        this.supperUserService = supperUserService;
+    }
+    /**
+     * 课程管理,从主页跳转
+     */
+    @RequestMapping("selectClass")
+    public String selectClass(){
+        return "redirect:seeClass";
+    }
+    //展示课程
+    @RequestMapping("seeClass")
+    public String seeClass(Model model){
+        List<MyClass> classList = supperUserService.selectClass();
+        model.addAttribute("classList",classList);
+        return "supperClassPage";
+    }
+    
 }
